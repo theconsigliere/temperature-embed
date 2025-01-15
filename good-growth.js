5cd932a5199f9b9c04b80a5f9d036dfb2bca35db
Dirty Martini <hello@maxwellkirwin.co.uk>
2025-01-15 19:33:38 +0000

    push

--- good-growth.js
+++ good-growth.js
@@ -9,7 +9,12 @@ const getTemperature = async () => {
   ).catch(handleError)
 
   const data = await response.json()
-  const result = data.list[0]
+
+  const result = {
+    degreeTemp: data.list[0].main.temp.toFixed(1),
+    description: data.list[0].weather[0].description,
+    icon_url: `https://openweathermap.org/img/w/${data.list[0].weather[0].icon}.png`,
+  }
 
   return result
 }
@@ -21,10 +26,8 @@ const handleError = () => {
 const createWidget = (temperature) => {
   const widget = document.createElement("div")
 
-  const degreeTemp = temperature.main.temp.toFixed(1)
-  const weather = temperature.weather[0]
-  const description = weather.description
-  const icon_url = `https://openweathermap.org/img/w/${weather.icon}.png`
+  // add all const to session storage
+  sessionStorage.setItem("temp", JSON.stringify(temperature))
 
   widget.classList.add("temperature-widget")
   widget.innerHTML = ``
@@ -32,9 +35,9 @@ const createWidget = (temperature) => {
         <div class="twidget" style="padding: 0 10px;background-color:black;margin:5px 0;width:fit-content;">
                 <div className="twidget__flex" style="display: flex; justify-content: flex-start; align-items: center;">
                    <p class="twidget__description" style="margin:0;color:white;font-size: 15px;">currently: </p>
-                    <p class="twidget__temp" style="margin:0;color:white;margin:0 10px;" >${degreeTemp}°C</p>
-                    <img class="twidget__icon" src="${icon_url}" width="22px" height="22px" style="margin:10px; "/>
-                    <p class="twidget__description" style="margin:0;color:white;font-size: 15px;">${description}</p>
+                    <p class="twidget__temp" style="margin:0;color:white;margin:0 10px;" >${temperature.degreeTemp}°C</p>
+                    <img class="twidget__icon" src="${temperature.icon_url}" width="22px" height="22px" style="margin:10px; "/>
+                    <p class="twidget__description" style="margin:0;color:white;font-size: 15px;">${temperature.description}</p>
                 </div>
         </div>
     `
@@ -42,8 +45,17 @@ const createWidget = (temperature) => {
 }
 
 const init = async () => {
-  const temperature = await getTemperature()
-  const widget = createWidget(temperature)
+  // check it session storage has temp
+  const temp = sessionStorage.getItem("temp")
+  let widget = null
+
+  if (temp) {
+    widget = createWidget(JSON.parse(temp))
+  } else {
+    const temperature = await getTemperature()
+    widget = createWidget(temperature)
+  }
+
   div.appendChild(widget)
 }
 
